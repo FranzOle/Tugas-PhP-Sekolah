6706fae170f9802cfaaff10c89ac7888f3f62fda
Lionel jevon <lioneljevon1212@gmail.com>
2024-01-20 09:33:06 +0700

    Update 4 zodiac
    Perubahan

--- Tugas Zodiak/zodiac.php
+++ Tugas Zodiak/zodiac.php
@@ -6,6 +6,7 @@
 </form>
 
 <?php 
+// Dikumpulkan pada 14 Januari hanya melakukan sedikit perubahan
 if (isset($_POST["bulan"]) && isset($_POST["tanggal"])){
 $tanggal = (int)$_POST["tanggal"] ;
 $bulan = (int)$_POST["bulan"] ;
@@ -53,48 +54,48 @@ if ($bulan === 1) {
 } else if ($bulan === 6) {
     if ($tanggal > 0 && $tanggal <= 18) {
         $zodiak = "Sagittarius";
-    } else if ($tanggal >= 18 && $tanggal < 31) {
+    } else if ($tanggal > 18 && $tanggal < 31) {
         $zodiak = "Scorpio";
     }
 } else if ($bulan === 7) {
     if ($tanggal > 0 && $tanggal <= 21) {
         $zodiak = "Leo";
-    } else if ($tanggal >= 21 && $tanggal <= 31) {
+    } else if ($tanggal > 21 && $tanggal < 31) {
         $zodiak = "Aries";
     }
 } else if ($bulan === 8) {
     if ($tanggal > 0 && $tanggal <= 17) {
         $zodiak = "Zeus";
-    } else if ($tanggal >= 17 && $tanggal < 31) {
+    } else if ($tanggal > 17 && $tanggal <= 31) {
         $zodiak = "Pisces";
     }
 } else if ($bulan === 9) {
     if ($tanggal > 0 && $tanggal <= 24) {
         $zodiak = "Libra";
-    } else if ($tanggal >= 24 && $tanggal < 31) {
+    } else if ($tanggal > 24 && $tanggal < 31) {
         $zodiak = "Capricorn";
     }
 } else if ($bulan === 10) {
     if ($tanggal > 0 && $tanggal <= 26) {
         $zodiak = "Leo";
-    } else if ($tanggal >= 26 && $tanggal < 31) {
+    } else if ($tanggal > 26 && $tanggal <= 31) {
         $zodiak = "Zeus";
     }
 } else if ($bulan === 11) {
     if ($tanggal > 0 && $tanggal <= 19) {
         $zodiak = "Scorpio";
-    } else if ($tanggal >= 19 && $tanggal < 31) {
+    } else if ($tanggal > 19 && $tanggal < 31) {
         $zodiak = "Gemini";
     }
 } else if ($bulan === 12) {
     if ($tanggal > 0 && $tanggal <= 21) {
         $zodiak = "Aquarius";
-    } else if ($tanggal >= 21 && $tanggal < 31) {
+    } else if ($tanggal > 21 && $tanggal <= 31) {
         $zodiak = "Gemini";
     } 
 } }
 
-echo $zodiak;
+echo '<p style="color:red;">' . $zodiak . '</p>';
 }
 
 ?>
